cbefd927944b2597eb44c3fa722f55865ace74da
toshmusicjp <134839550+toshmusicjp@users.noreply.github.com>
2025-03-26 14:06:09 +0900

    Update jackorrich.py

--- jackorrich.py
+++ jackorrich.py
@@ -71,11 +71,11 @@ class App:
                 self.is_game_over = True
 
         # 操作判定
-        if pyxel.btn(pyxel.KEY_LEFT) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_DPAD_LEFT, 8, 2):
+        if pyxel.btn(pyxel.KEY_LEFT) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_LEFT):
             if -3 < self.dx:
                 self.dx -= 1
             self.pldir = -1
-        elif pyxel.btn(pyxel.KEY_RIGHT) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_DPAD_RIGHT, 8, 2):
+        elif pyxel.btn(pyxel.KEY_RIGHT) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_RIGHT):
             if self.dx < 3:
                 self.dx += 1
             self.pldir = 1
